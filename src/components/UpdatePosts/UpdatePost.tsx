0b67d03754b1b3a5c03d5798454c06a6a02befe5
Lucas Goldman <Lucas.Goldman99@gmail.com>
2022-10-26 14:07:34 -0400

    fixed update site to be re-arrangeable

--- src/components/UpdatePosts/UpdatePost.tsx
+++ src/components/UpdatePosts/UpdatePost.tsx
@@ -1,12 +1,11 @@
 import { PostProps } from "../Timeline/Post";
-import React, { useState } from "react";
+import React, { SyntheticEvent, useImperativeHandle, useState } from "react";
 import { PostInputUpdate } from "./PostInputUpdate";
 import { Flexbox } from "../../styling";
 import styled from "styled-components";
 
 type UpdatePostsProps = {
   post: PostProps;
-  onChange: Function;
 };
 
 type postKeys = keyof PostProps;
@@ -15,30 +14,44 @@ const ColumnFlexbox = styled(Flexbox)`
   flex-direction: column;
 `;
 
-export const UpdatePost: React.FC<UpdatePostsProps> = ({ post, onChange }) => {
-  const [updateVisible, setUpdateVisible] = useState(false);
+export const UpdatePost = React.forwardRef<PostProps, UpdatePostsProps>(
+  ({ post }, ref: React.Ref<PostProps>) => {
+    const [updateVisible, setUpdateVisible] = useState(false);
+    const [postState, setPostState] = useState<PostProps>(post);
 
-  const postUpdateInputsByKeys = (
-    <ColumnFlexbox>
-      {(Object.keys(post) as Array<postKeys>).map(
-        (postKey: postKeys, index) => {
-          return (
-            <PostInputUpdate
-              key={postKey + index}
-              postKey={postKey}
-              content={post[postKey]}
-              onChange={onChange}
-            />
-          );
-        }
-      )}
-    </ColumnFlexbox>
-  );
+    useImperativeHandle(ref, () => postState, []);
 
-  return (
-    <>
-      <h1 onClick={() => setUpdateVisible(!updateVisible)}>{post.header}</h1>
-      {updateVisible && postUpdateInputsByKeys}
-    </>
-  );
-};
+    const handlePostUpdate = (
+      postKey: postKeys,
+      event: SyntheticEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+      const newPost: PostProps = postState;
+      newPost[postKey] = event.currentTarget.value;
+      setPostState(newPost);
+    };
+
+    const postUpdateInputsByKeys = (
+      <ColumnFlexbox>
+        {(Object.keys(post) as Array<postKeys>).map(
+          (postKey: postKeys, index) => {
+            return (
+              <PostInputUpdate
+                key={postKey + index}
+                postKey={postKey}
+                content={post[postKey]}
+                onChange={handlePostUpdate}
+              />
+            );
+          }
+        )}
+      </ColumnFlexbox>
+    );
+
+    return (
+      <>
+        <h1 onClick={() => setUpdateVisible(!updateVisible)}>{post.header}</h1>
+        {updateVisible && postUpdateInputsByKeys}
+      </>
+    );
+  }
+);
--- src/pages/UpdateSite.tsx
+++ src/pages/UpdateSite.tsx
@@ -1,4 +1,4 @@
-import React, { SyntheticEvent } from "react";
+import React, { SyntheticEvent, useRef } from "react";
 import read from "../services/read";
 import post from "../services/post";
 import firebaseResources from "../services/firebaseResources";
@@ -11,9 +11,6 @@ import {
   UpdatePost,
 } from "../components";
 import styled from "styled-components";
-import { InputLabel, MenuItem, Select } from "@mui/material";
-import { postSelectOptions } from "../components/UpdatePosts/postSelectOptions";
-import { sortPostsByYear } from "../utils";
 import { Draggable } from "react-drag-reorder";
 
 type postKeys = keyof PostProps;
@@ -24,16 +21,10 @@ const UpdateSiteContainer = styled.div`
   gap: 20px;
 `;
 
-//I need to map all the objects in the array.
-//Each object has a header
-//display a list of headers - when clicked on they reveal the update properties of the obj
-//each header is drag/droppable w/ library
-
 export const UpdateSite = () => {
   const [posts, setPosts] = useState<PostProps[]>([
     { content: "", header: "" },
   ]);
-  const [selectedPost, setSelectedPost] = useState<number>(0);
 
   useEffect(() => {
     read(firebaseResources.home).then((posts: PostProps[]) => {
@@ -42,60 +33,16 @@ export const UpdateSite = () => {
   }, []);
 
   const handleUpdateSite = async () => {
-    await post(firebaseResources.home, posts);
+    await post(firebaseResources.home, refs.current);
     window.location.reload();
   };
 
-  const handlePostUpdate = (
-    postKey: postKeys,
-    event: SyntheticEvent<HTMLInputElement | HTMLTextAreaElement>,
-    selectedPost: number
-  ) => {
-    const newPosts: PostProps[] = [...posts];
-    newPosts[selectedPost][postKey] = event.currentTarget.value;
-    setPosts(newPosts);
-    console.log(newPosts);
-  };
-
-  // const postUpdateInputsByKeys = (
-  //   Object.keys(posts[selectedPost]) as Array<postKeys>
-  // ).map((postKey: postKeys, index) => {
-  //   return (
-  //     <PostInputUpdate
-  //       key={postKey + index}
-  //       postKey={postKey}
-  //       content={posts[selectedPost][postKey]}
-  //       onChange={handlePostUpdate}
-  //     />
-  //   );
-  // });
-
-  // const handleSelectedPostChange = (updatedSelectedPost: number) => {
-  //   if (updatedSelectedPost === posts.length) {
-  //     setPosts((prevState) => [
-  //       ...prevState,
-  //       { content: "", header: "", technology: "" },
-  //     ]);
-  //   }
-  //   setSelectedPost(updatedSelectedPost);
-  // };
+  const refs = useRef(new Array());
 
   return (
     <PageContainer>
       <UpdateSiteContainer>
         <h1>Update Site</h1>
-        {/* <InputLabel id="demo-simple-select-label">Selected Post</InputLabel> */}
-        {/* <Select
-          id="demo-simple-select"
-          value={selectedPost}
-          onChange={(e) =>
-            handleSelectedPostChange(parseInt("" + e.target.value))
-          }
-          sx={{ color: "black" }}
-        >
-          {postSelectOptions(posts)}
-          <MenuItem value={posts.length}>Write a new post</MenuItem>
-        </Select> */}
         <Draggable
           onPosChange={(currentPos, newPos) => {
             const prevPosts = posts;
@@ -103,23 +50,29 @@ export const UpdateSite = () => {
             prevPosts[currentPos] = prevPosts[newPos];
             prevPosts[newPos] = temp;
             setPosts(prevPosts);
-            console.log(posts);
           }}
         >
           {posts.map((post, i) => (
             <UpdatePost
               post={post}
-              onChange={(
-                postKey: postKeys,
-                e: SyntheticEvent<HTMLInputElement | HTMLTextAreaElement>
-              ) => {
-                handlePostUpdate(postKey, e, i);
+              ref={(element) => {
+                refs.current.push(element);
+                if (refs.current.length > posts.length) {
+                  refs.current.shift();
+                }
               }}
             />
           ))}
         </Draggable>
         <button style={{ color: "black" }} onClick={() => handleUpdateSite()}>
           update
+        </button>
+        <button
+          onClick={() => {
+            console.log(refs.current);
+          }}
+        >
+          Log ref
         </button>
       </UpdateSiteContainer>
     </PageContainer>
